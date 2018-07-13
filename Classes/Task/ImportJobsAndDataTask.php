5bd8334114db573e84d4c019705568215337039c
anjeylink <andriy.oprysko@resultify.se>
2018-07-13 11:08:24 +0300

    [TASK] added additional information for scheduler task

--- Classes/Task/ImportJobsAndDataTask.php
+++ Classes/Task/ImportJobsAndDataTask.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 namespace Pixelant\PxaIntelliplanJobs\Task;
 
 use Pixelant\PxaIntelliplanJobs\Services\IntelliplanImportService;
+use TYPO3\CMS\Backend\Utility\BackendUtility;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Lang\LanguageService;
 use TYPO3\CMS\Scheduler\Task\AbstractTask;
 
 /**
@@ -47,5 +49,22 @@ class ImportJobsAndDataTask extends AbstractTask
     public function setPid(int $pid)
     {
         $this->pid = $pid;
+    }
+
+    public function getAdditionalInformation()
+    {
+        /** @var LanguageService $lang */
+        $lang = $GLOBALS['LANG'];
+
+        $page = BackendUtility::getRecord('pages', $this->getPid(), 'title');
+
+        return sprintf(
+            '%s: %s[%d]',
+            $lang->sL(
+                'LLL:EXT:pxa_intelliplan_jobs/Resources/Private/Language/locallang_db.xlf:importJobsAndDataTask.pid'
+            ),
+            $page['title'],
+            $this->getPid()
+        );
     }
 }
